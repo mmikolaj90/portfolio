291cf50493733680157aad161c8532abcc51ac02
Rath-san <rim.kozlowski@gmail.com>
2024-06-03 14:50:25 +0200

    Update social link and analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 // import AutoImport from 'astro-auto-import';
 import mdx from "@astrojs/mdx";
-
-// import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap";
 import lottie from "astro-integration-lottie";
+import partytown from '@astrojs/partytown'
 import { SITE_URL } from "./src/consts";
 
 // https://astro.build/config
@@ -11,12 +11,12 @@ export default defineConfig({
   site: SITE_URL,
   integrations: [
     lottie(),
-    // AutoImport({
-    //   imports: [
-    //     // Add your Note component to the auto-imports:
-    //     './src/components/BaseBlogImage.astro',
-    //   ],
-    // }),
     mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
   ],
 });
