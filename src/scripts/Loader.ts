e310204668a77f76f8b163d5a23abd7b3000f0e3
Rafał Kozłowski <rim.kozlowski@gmail.com>
2024-06-03 15:23:42 +0200

    Fixing close lottie animations

--- src/scripts/Loader.ts
+++ src/scripts/Loader.ts
@@ -4,18 +4,20 @@ const loader = () => {
   document.body.classList.add("loading");
 
   const animInit = () => {
-    const logo = document.getElementById("logo")!;
-    const logoConfig = JSON.parse(logo?.getAttribute("data-lottie-data")!);
+    const logos = document.querySelectorAll(".anim--logo")!;
+    logos.forEach((logo) => {
+      const logoConfig = JSON.parse(logo?.getAttribute("data-lottie-data")!);
 
-    Lottie.loadAnimation({
-      container: logo, // the dom element that will contain the animation
-      renderer: "svg",
-      loop: logoConfig.loop ?? false,
-      autoplay: logoConfig.autoplay ?? false,
-      path: logoConfig.src, // the path to the animation json
-      rendererSettings: {
-        viewBoxOnly: true,
-      },
+      Lottie.loadAnimation({
+        container: logo, // the dom element that will contain the animation
+        renderer: "svg",
+        loop: logoConfig.loop ?? false,
+        autoplay: logoConfig.autoplay ?? false,
+        path: logoConfig.src, // the path to the animation json
+        rendererSettings: {
+          viewBoxOnly: true,
+        },
+      });
     });
   };
 
